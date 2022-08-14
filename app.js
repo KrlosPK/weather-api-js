3a190af7de79db7fe8125eb9fd71c5644629125d
KrlosPK <centille095@gmail.com>
2022-08-14 15:43:44 -0500

    Mejoras en la interfaz

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ let weather = {
             document.querySelector(".humedad").innerText = "";
             document.querySelector(".viento").innerText = "";
             document.body.style.backgroundImage =
-                "url('https://source.unsplash.com/1600x900/?404&not&found')";
+                "url('https://images.unsplash.com/photo-1584824486509-112e4181ff6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')";
         }
     },
     search: function () {
